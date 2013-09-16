4c4ca5c414d316a3448de29917f489e5ae7ea19e
Brendan McLoughlin <btmcloughlin@gmail.com>
2013-09-16 10:54:53 -0400

    Update gaia_remotes.sh

--- gaia_remotes.sh
+++ gaia_remotes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-for remote in evhan55 gnarf jugglinmike oconnore protonk rwaldron rwldrn
+for remote in evhan55 gnarf jugglinmike oconnore rwaldron bmac tafsiri
 do
-  git remote add $remote https://github.com/$remote/gaia
+  git remote add $remote https://github.com/$remote/gaia.git
 done
